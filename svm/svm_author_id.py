af7867cee0156b13adff6dddf2bc588bbf023f32
sandeshkaranam <sandeshkaranam1@gmail.com>
2016-10-11 15:48:00 +0530

    SVM mini assignment from udacity

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,19 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
-#########################################################
-### your code goes here ###
-
-#########################################################
-
-
+from sklearn.svm import SVC
+clf = SVC(C=10000.0,kernel='rbf')
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+predicted_set = clf.predict(features_test)
+count_chris=0
+for pe in predicted_set:  
+    if(pe==1) :
+        count_chris = count_chris+1 
+print(count_chris)
+print "predict time:", round(time()-t0, 3), "s"
+print(clf.score(features_test,labels_test))
