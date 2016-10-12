b8d85a379c8079e107e37147bad5a55de859e687
sandeshkaranam <sandeshkaranam1@gmail.com>
2016-10-12 07:17:13 +0530

    Decision tress assignment by  Udacity

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,8 +19,11 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+print clf.score(features_test,labels_test)
+print len(features_train[0])
 
 #########################################################
 ### your code goes here ###
